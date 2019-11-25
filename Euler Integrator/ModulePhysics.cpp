282774190cd656da6ee1730a374901fa7c441eb9
MarcArizaAlborni <45202074+MarcArizaAlborni@users.noreply.github.com>
2019-11-25 20:53:10 +0100

    rebote amb sort

--- Euler Integrator/ModulePhysics.cpp
+++ Euler Integrator/ModulePhysics.cpp
@@ -62,10 +62,11 @@ void ModulePhysics::EulerIntegrator(vec3d & iposition, vec3d & ivelocity, vec3d 
 			ivelocity.x = 0;
 			fvelocity.x = 0;
 			acceleration.x=0;
+			
 		}
 
-		if (fposition.x < 924 ) {
-			if (fposition.y > 650)
+		if (fposition.x <= 924 ) {
+			if (fposition.y >= 650)
 			{
 				fposition.y = 650;
 
@@ -89,7 +90,7 @@ void ModulePhysics::EulerIntegrator(vec3d & iposition, vec3d & ivelocity, vec3d 
 		}
 		
 
-	    if (fposition.x > 924 ){
+	    if (fposition.x >= 924 ){
 				LOG("REBOUND RIGHT TO LEFT ");
 			
 
@@ -98,7 +99,7 @@ void ModulePhysics::EulerIntegrator(vec3d & iposition, vec3d & ivelocity, vec3d 
 				iposition.y = fposition.y;							//Resets the object's initial position in the Y axis to the new position.
 				iposition.z = fposition.z;							//Resets the object's initial position in the Y axis to the new position.
 
-				fvelocity.x = -(ivelocity.x - acceleration.x * dt) ; //DRAG FORCE 0.9;	//Gets the object's final velocity in the X axis.
+				fvelocity.x = (ivelocity.x - acceleration.x * dt)*(-0.7) ; //DRAG FORCE 0.9;	//Gets the object's final velocity in the X axis.
 				if (fvelocity.y < 0) {
 
 					fvelocity.y = 0;
@@ -110,7 +111,7 @@ void ModulePhysics::EulerIntegrator(vec3d & iposition, vec3d & ivelocity, vec3d 
 				ivelocity.y = fvelocity.y;							//Resets the object's initial velocity in the Y axis to the new velocity.
 				ivelocity.z = fvelocity.z;							//Resets the object's initial velocity in the Z axis to the new velocity.
 		}
-		else if (fposition.x < 0) {
+		else if (fposition.x <= 0) {
 			fposition.y = 650;
 			LOG("REBOUND LEFT TO RIGHT");
 
@@ -118,7 +119,7 @@ void ModulePhysics::EulerIntegrator(vec3d & iposition, vec3d & ivelocity, vec3d 
 			iposition.y = fposition.y;							//Resets the object's initial position in the Y axis to the new position.
 			iposition.z = fposition.z;							//Resets the object's initial position in the Y axis to the new position.
 
-			fvelocity.x = -(ivelocity.x + acceleration.x * dt) *(0.8); //DRAG FORCE 0.9;	//Gets the object's final velocity in the X axis.
+			fvelocity.x = -(ivelocity.x + acceleration.x * dt) *(0.7); //DRAG FORCE 0.9;	//Gets the object's final velocity in the X axis.
 			if (fvelocity.y < 0) {
 
 				fvelocity.y = 0;
